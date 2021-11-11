0d93813cbb9c455d2f17e38371ae2c287281036f
Théo Pomies <theo.pomies@outlook.com>
2021-11-11 19:46:39 +0100

    adapted for vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,17 @@
+import vercel from "@sveltejs/adapter-vercel";
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-		vite: {
-			define: {
-				global: 'window'
-			}
-		}
-	}
+  kit: {
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: "#svelte",
+    adapter: vercel(),
+    vite: {
+      define: {
+        global: "window",
+      },
+    },
+  },
 };
 
 export default config;
